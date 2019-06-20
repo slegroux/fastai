f8c25f57ce48df59b33935aa0688bfeac0b6fa5f
Sylvain Le Groux <sylvain.legroux@fulbrightmail.org>
2019-06-20 13:30:00 -0700

    split line based data for training

--- slg/data/split_train_dev_test.sh
+++ slg/data/split_train_dev_test.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
 file=$1
+name=$(basename $file)
+bn=${name%.*}
 length=$(cat $file | wc -l)
 l_train=$(echo "$length * 0.7" |bc|xargs printf "%.0f")
 l_dev=$(echo "$length * 0.2" |bc|xargs printf "%.0f")
 l_test=$(echo "$length * 0.1" |bc|xargs printf "%.0f")
 
-echo $l_train, $l_dev, $l_test
+file_r=$(shuf --random-source=$file $file)
+echo "train:" $l_train "dev:" $l_dev "test:" $l_test
+echo "$file_r" | head -n $l_train > $bn.train.txt
+echo "$file_r" | tail -n +$(( $l_train + 1 ))| head -n $l_dev > $bn.dev.txt
+echo "$file_r" | tail -n $l_test > $bn.test.txt
+
+
